5c5feb730357b3cde794a0861bc4fa3c75ef2f8f
juansj2000@gmail.com <juansj2000@gmail.com>
2026-06-07 02:15:58 +0200

    Improve site visuals: sticky header, card shadows, hover effects, mobile fixes

--- site/themes/sanpablo/assets/js/main.js
+++ site/themes/sanpablo/assets/js/main.js
@@ -1,6 +1,26 @@
 (function () {
   'use strict';
 
+  // Sticky header scroll detection
+  const header = document.querySelector('.site-header');
+  if (header) {
+    let ticking = false;
+    function updateHeader() {
+      if (window.scrollY > 10) {
+        header.classList.add('is-scrolled');
+      } else {
+        header.classList.remove('is-scrolled');
+      }
+      ticking = false;
+    }
+    window.addEventListener('scroll', function () {
+      if (!ticking) {
+        window.requestAnimationFrame(updateHeader);
+        ticking = true;
+      }
+    });
+  }
+
   const navToggle = document.querySelector('.nav-toggle');
   const mainNav = document.getElementById('main-nav');
 
